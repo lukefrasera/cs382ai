00762d7ae123213a75e49c84230ad89204c3a68d
Luke Fraser <totalvideopro@gmail.com>
2013-09-29 01:49:50 -0700

    adding classes and creating main driver

---
+++ A2/src/mainDriver.py
@@ -0,0 +1,41 @@
+# MainDriver.py
+# import dirplay class
+import sys, os
+from mapGenerator import Map
+# IMPORT DISPLAY CLASS
+
+def cls():
+    os.system(['clear','cls'][os.name == 'nt'])
+
+def main():
+	#main program loop
+	while(True):
+		# Generate Terrain
+		# clear console
+		cls()
+		# Generate menu cases
+		input = raw_input("Choose Algorithm:\n\
+							1) BFS   :\n\
+							2) A*    :\n\
+							3) Greedy:\n\
+							4) UCS   :\n\
+							5) Hill  :\n\
+							6) Quit  :\n")
+			# BFS
+		if(input == '1'):
+			plot.runBFS(w,h,table)
+			# A*
+		if*input == '2'):
+			plot.runStar(w,h,table)
+			# Greedy
+		if(input == '3'):
+			plot.runGreed(w,h,table)
+			# uniform cost search
+		if(input == '4'):
+			plot.runUCS(w,h,table)
+			# hill climbing search
+		if(input == '5'):
+			plot.runHill(w,h,table)
+
+if __name__ == '__main__':
+	main()
---
+++ A2/src/mapGenerator.py
@@ -0,0 +1,72 @@
+import random
+
+class Map:
+    def __init__(self, l, w):
+        '''Creates a blank map of a 2D world without obstactles. l columns and w rows'''
+        self.maxRows = w
+        self.maxCols = l
+        self.blank()
+
+    def blank(self):
+        self.map = [[0 for x in range (self.maxCols)] for x in range(self.maxRows)]
+
+    def makeRectObstacle(self):
+        '''Make a random rectangular obstacle'''
+        row = random.randint(0, self.maxRows - 2)
+        col = random.randint(0, self.maxCols - 2)
+        height = random.randint(1, self.maxRows - 1 - row)
+        width  = random.randint(1, self.maxCols - 1 - col)
+
+        self.changeMap(row, col, height, width)
+        return (row, col, height, width)
+        
+        
+    def changeMap(self, row, col, height, width):
+        '''Marks up a rectangle in our 2D map'''
+        for x in range (row, row+height):
+            for y in range(col, col+width):
+                self.map[x][y] += 1
+
+    def path(self, path):
+        pass
+
+    def copyWorldMap(self, anotherWorld):
+        '''Copies world, obstacles and all'''
+        for r in range(self.maxRows):
+            for c in range(self.maxCols):
+                self.map[r][c] = anotherWorld.map[r][c]
+
+    def display(self, stdscr):
+        '''Display for curses'''
+        for l in range(self.maxRows):
+            for w in range(self.maxCols):
+                if self.map[l][w] < 0:
+                    stdscr.addch(l, w, '.') # expanded
+                elif self.map[l][w] == 9:
+                    stdscr.addch(l, w, '0') # path
+                elif self.map[l][w] > 0:
+                    stdscr.addch(l, w, '+') # obstacle
+
+    def __repr__(self):
+        '''For printing'''
+        out = ''
+        for l in range(self.maxRows):
+            for w in range(self.maxCols):
+                if self.map[l][w] == 0:
+                    out = out + ' '
+                elif self.map[l][w] < 0:
+                    out = out + str(-self.map[l][w])
+                else:
+                    out = out + str(self.map[l][w])
+            out = out + " |\n"
+        return out
+                
+# For testing this code
+if __name__ == "__main__" :
+    random.seed()
+    maxCols = 80
+    maxRows = 30
+    world = Map(maxCols, maxRows)
+    for x in range(5):
+        tmp = world.makeRectObstacle()
+    print world
